acddfb25ff933213ef32da1f55759e558a68c122
Felipe Hoffmann <felipejh@gmail.com>
2024-04-26 07:22:02 -0300

    feat: add home screen

--- AppBackFront/AppBackFront/Feature/Home/VC/HomeVC.swift
+++ AppBackFront/AppBackFront/Feature/Home/VC/HomeVC.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    var screen: HomeScreen?
 
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
