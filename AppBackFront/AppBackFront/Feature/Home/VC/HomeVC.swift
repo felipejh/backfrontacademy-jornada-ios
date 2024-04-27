5e16537f326df5598327baaa4a453f0bf06e1f26
Felipe Hoffmann <felipejh@gmail.com>
2024-04-27 09:29:17 -0300

    Aula 141

--- AppBackFront/AppBackFront/Feature/Home/VC/HomeVC.swift
+++ AppBackFront/AppBackFront/Feature/Home/VC/HomeVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class HomeVC: UIViewController {
     
     var screen: HomeScreen?
+    var viewModel = HomeViewModel()
 
     override func loadView() {
         screen = HomeScreen()
@@ -18,10 +19,25 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel.delegate(delegate: self)
+        viewModel.fetchRequest(.request)
 
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
     }
+}
+
+extension HomeVC: HomeViewModelDelegate {
+    func success() {
+        print(#function)
+    }
+    
+    func error() {
+        print(#function)
+    }
+    
+    
 }
--- AppBackFront/AppBackFront/Services/HomeService/HomeService.swift
+++ AppBackFront/AppBackFront/Services/HomeService/HomeService.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import Alamofire
 
 protocol HomeServiceDelegate: GenericService {
     func getHomeFromJson(completion: @escaping completion<NFTData?>)
+    func getHome(completion: @escaping completion<NFTData?>)
 }
 
 class HomeService: HomeServiceDelegate {
 
+    func getHome(completion: @escaping completion<NFTData?>) {
+        let url: String = "https://run.mocky.io/v3/07745758-5fa4-4d6d-850f-f02d616efc44"
+        
+        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of: NFTData.self) { response in
+            debugPrint(response)
+            
+            switch response.result {
+            case .success(let success):
+                print("SUCCESS -> \(#function)")
+                completion(success, nil)
+            case .failure(let error):
+                print("ERROR -> \(#function)")
+                completion(nil, Error.errorRequest(error))
+            }
+        }
+    }
+    
     func getHomeFromJson(completion: @escaping completion<NFTData?>) {
         if let url = Bundle.main.url(forResource: "HomeData", withExtension: "json") {
             do {
