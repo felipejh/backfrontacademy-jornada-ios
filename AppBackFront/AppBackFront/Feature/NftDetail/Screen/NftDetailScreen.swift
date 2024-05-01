712441bf397d77d90c89a0e2e2fdd0be17851258
Felipe Hoffmann <felipejh@gmail.com>
2024-05-01 08:31:13 -0300

    Aula 148

---
+++ AppBackFront/AppBackFront/Feature/NftDetail/Screen/NftDetailScreen.swift
@@ -0,0 +1,52 @@
+//
+//  NftDetailScreen.swift
+//  AppBackFront
+//
+//  Created by Felipe Hoffmann on 01/05/24.
+//
+
+import UIKit
+
+class NftDetailScreen: UIView {
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // TO DO: Register
+        tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        tableView.separatorStyle = .none
+        
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addElements()
+        configConstraints()
+    }
+    
+    public func configTableViewProtocols(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addElements() {
+        addSubview(tableView)
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+        ])
+    }
+}
---
+++ AppBackFront/AppBackFront/Feature/NftDetail/ViewModel/NftDetailViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  NftDetailViewModel.swift
+//  AppBackFront
+//
+//  Created by Felipe Hoffmann on 01/05/24.
+//
+
+import UIKit
+
+class NftDetailViewModel {
+
+    private let nft: Nft
+    
+    init(nft: Nft) {
+        self.nft = nft
+    }
+}
