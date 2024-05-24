cf823dc044ce9a1de69d2b1729fc8653412e5977
Felipe Hoffmann <felipejh@gmail.com>
2024-05-24 07:51:27 -0300

    Criando tela de adicionar notas

--- SwiftUI/NotesApp/NotesApp/Notes/NotesView.swift
+++ SwiftUI/NotesApp/NotesApp/Notes/NotesView.swift
@@ -7,16 +7,53 @@
 
 import SwiftUI
 
+//  @ObservedObject é usado em uma View para reagir a mudanças em um ObservableObject.
+//  (Quem escuta)
+
+//  @StateObject é simmilar ao @ObservedObject, mas é responsável pela criação e destruição
+//  do objeto observado, sendo mais apropriado para objetos que a view deve possuir. (Também escuta)
+
+//  @Published é utilizado para notificar mudanças em propriedades de um ObservableObject
+//  É quem vai gritar e todos vão escutar: StateObject e ObservedObject
+
 struct NotesView: View {
     
     @StateObject var viewModel = NotesViewModel()
+    @State var isGoAddNote: Bool = false
     
     var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         List($viewModel.notes, editActions: .all) { $note in
-            Text(note.title)
-            Text(note.content)
+            NavigationLink {
+                Color.red
+            } label: {
+                HStack {
+                    Image(systemName: "pencil")
+                        .frame(width: 24, height: 24)
+                        .padding(.trailing, 8)
+                    VStack(alignment: .leading) {
+                        Text(note.title)
+                            .font(.headline)
+                        Text(note.content)
+                            .font(.subheadline)
+                    }
+                }
+            }
         }
+        .navigationBarBackButtonHidden()
+        .navigationTitle("Notas")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Adicionar") {
+                    isGoAddNote.toggle()
+                }
+            }
+        }
+//        .navigationDestination(isPresented: $isGoAddNote) {
+//            Color.blue
+//        }
+        .sheet(isPresented: $isGoAddNote, content: {
+            AddNotesView(viewModel: viewModel)
+        })
     }
 }
 
--- SwiftUI/NotesApp/NotesApp/Notes/ViewModel/NotesViewModel.swift
+++ SwiftUI/NotesApp/NotesApp/Notes/ViewModel/NotesViewModel.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+//  @ObservedObject é usado em uma View para reagir a mudanças em um ObservableObject.
+//  (Quem escuta)
+
+//  @StateObject é simmilar ao @ObservedObject, mas é responsável pela criação e destruição
+//  do objeto observado, sendo mais apropriado para objetos que a view deve possuir. (Também escuta)
+
+//  @Published é utilizado para notificar mudanças em propriedades de um ObservableObject
+//  É quem vai gritar e todos vão escutar: StateObject e ObservedObject
+
 class NotesViewModel: ObservableObject {
     @Published var notes: [Note] = [
         Note(title: "Título", content: "Descrição do conteúdo")
