ec83485c5d91ee8788d4c433b47f7eab0fd58352
Felipe Hoffmann <felipejh@gmail.com>
2024-08-10 11:49:40 -0400

    CustomVideo

--- SwiftUI/BackfrontPosts/BackfrontPosts/CustomVideoView/CustomVideoView.swift
+++ SwiftUI/BackfrontPosts/BackfrontPosts/CustomVideoView/CustomVideoView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct CustomVideoView: View {
     
-    @State var videoURL: URL?
+    var videoURL: URL?
+    @State var player = AVPlayer()
     @Binding var isMuted: Bool
     
     init(url: String, isMuted: Binding<Bool>) {
@@ -19,10 +21,39 @@ struct CustomVideoView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let url = videoURL {
+            ZStack(alignment: .bottomTrailing) {
+                VideoPlayer(player: player)
+                    .onAppear {
+                        player = AVPlayer(url: url)
+                        player.play()
+                        player.isMuted = isMuted
+                    }
+                Color.white.opacity(0)
+                Button(action: {
+                    isMuted.toggle()
+                }, label: {
+                    Image(systemName: isMuted ? "speaker.slash.fill" : "speaker.2.fill")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(Color.black.opacity(0.7))
+                        .clipShape(Circle())
+                })
+            }
+        } else {
+            VStack{
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .resizable()
+                    .foregroundStyle(.yellow)
+                    .frame(width: 50, height: 50)
+                Text("Ops, tivemos um problema com esse vídeo.")
+                    .lineLimit(2)
+            }
+            
+        }
     }
 }
 
 #Preview {
-    CustomVideoView(url: "demo2", isMuted: .constant(true))
+    CustomVideoView(url: "cars", isMuted: .constant(true))
 }
